3a19e0922a66878b374d678474ed95c689daeb8d
Prince Emelife <princeemelife@gmail.com>
2022-03-07 18:50:44 +0100

    Media Tile foundation

---
+++ lib/screens/media_tile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NoteMediaTile extends StatelessWidget {
+  final bool isMedia;
+  final String? note;
+  final String? imageUrl;
+  const NoteMediaTile(
+      {Key? key, required this.isMedia, this.note, this.imageUrl})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
