da5738ef125e07b1af82306f7153112568fcd487
Prince Emelife <princeemelife@gmail.com>
2022-05-18 18:42:55 +0100

    Test file

--- test_file/bin/test_file.dart
+++ test_file/bin/test_file.dart
@@ -2,6 +2,45 @@ void main(List<String> arguments) {
   String test = '[What is your name?]';
   int len = test.length;
   String a = test.substring(1, len - 1);
+  Goat g = Goat('The Goat');
+  file() {
+    g.eat();
+  }
 
-  print(a);
+  print(file());
+}
+
+class Animal {
+  String name;
+  Animal({required this.name});
+
+  eat() {
+    print('$name is eating');
+  }
+
+  grow() {
+    print('$name is growing');
+  }
+
+  die() {
+    print('$name dies');
+  }
+
+  live() {
+    eat();
+    grow();
+    die();
+  }
+}
+
+class Dog extends Animal {
+  Dog(String name) : super(name: name);
+}
+
+class Cat extends Animal {
+  Cat(String name) : super(name: name);
+}
+
+class Goat extends Animal {
+  Goat(String name) : super(name: name);
 }
