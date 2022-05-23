2fd69a37909a76427826b4901d8736987e9f400c
Prince Emelife <princeemelife@gmail.com>
2022-05-23 22:27:35 +0100

    Minor refactors

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,10 +1,5 @@
-import 'package:flutter_slidable/flutter_slidable.dart';
-import 'package:pic_note/shared/styling/colors.dart';
-
 import 'imports.dart';
 import 'package:flutter/material.dart';
-
-import 'widgets/label_row.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -189,88 +184,4 @@ class _HomeState extends State<Home> {
           );
         });
   }
-}
-
-labelDialog(BuildContext context, Note note, int index) async {
-  PicDataBase picDataBase = Provider.of<PicDataBase>(context, listen: false);
-  List<String> tags = [];
-  tags = await picDataBase.getTags(index);
-  String tag = '';
-  TextEditingController textEditingController = TextEditingController();
-  await showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return StatefulBuilder(builder: (context, setState) {
-        /// Updates an existing [Note]
-
-        return AlertDialog(
-          title: const Center(
-              child: Text(
-            cUpdateLabel,
-          )),
-          content: SizedBox(
-            height: 160,
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  LabelRow(picDataBase: picDataBase, index: index, note: note),
-                  TextFormField(
-                    cursorColor: Theme.of(context).textTheme.headline1!.color,
-                    style: TextStyle(
-                        color: Theme.of(context).textTheme.headline1!.color),
-                    decoration: InputDecoration(
-                        hoverColor: Colors.white,
-                        counterStyle: TextStyle(
-                            color:
-                                Theme.of(context).textTheme.headline1!.color)),
-                    maxLength: 15,
-                    onChanged: (value) {
-                      tag = value;
-                    },
-                    controller: textEditingController,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      TextButton(
-                        onPressed: () {
-                          textEditingController.clear();
-                          Navigator.pop(context, cCamera);
-                        },
-                        child: const Text(cCancel),
-                      ),
-                      TextButton(
-                        autofocus: tag.trim().isNotEmpty,
-                        onPressed: () {
-                          if (tag.trim().isNotEmpty && tags.length < 6) {
-                            tags.add(tag);
-                            Note newNote = Note(
-                                id: note.id,
-                                title: note.title.toString(),
-                                date: DateTime.now(),
-                                subtitle: note.subtitle,
-                                tags: tags);
-                            Utils().update(newNote, picDataBase);
-                            tag = '';
-                            textEditingController.clear();
-                            setState(() {});
-                            Utils().logger(
-                                cHome, 'Tag list is ${tags.toString()}');
-                          } else {
-                            Utils().logger(cAlertDialog, cNullValue);
-                          }
-                          textEditingController.clear();
-                        },
-                        child: const Text(cAdd),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            ),
-          ),
-        );
-      });
-    },
-  );
 }
--- lib/imports.dart
+++ lib/imports.dart
@@ -1,34 +1,36 @@
 export 'dart:developer';
 
-export 'package:pic_note/edit_screen.dart';
+export 'edit_screen.dart';
 export 'models/note_model.dart';
 export 'shared/constants.dart';
 export 'shared/database/database.dart';
 export 'widgets/tiles.dart';
-export 'package:pic_note/home_page.dart';
-export 'package:pic_note/shared/database/database.dart';
+export 'home_page.dart';
+export 'shared/database/database.dart';
 export 'dart:developer';
 
 export 'package:path/path.dart';
-export 'package:pic_note/models/note_model.dart';
+export 'models/note_model.dart';
 export 'package:shared_preferences/shared_preferences.dart';
 export 'package:sqflite/sqflite.dart';
 
-export 'package:pic_note/shared/constants.dart';
-export 'package:pic_note/models/note_model.dart';
+export 'shared/constants.dart';
+export 'models/note_model.dart';
 export 'dart:developer';
 export 'dart:ui';
 
-export 'package:pic_note/home_page.dart';
-export 'package:pic_note/models/note_model.dart';
-export 'package:pic_note/shared/constants.dart';
-export 'package:pic_note/shared/database/database.dart';
+export 'home_page.dart';
+export 'models/note_model.dart';
+export 'shared/constants.dart';
+export 'shared/database/database.dart';
 export 'package:shared_preferences/shared_preferences.dart';
-export 'package:pic_note/models/note_model.dart';
-export 'package:pic_note/shared/utils.dart';
+export 'models/note_model.dart';
+export 'shared/utils.dart';
 export 'package:image_picker/image_picker.dart';
 
-export 'package:pic_note/widgets/bubble.dart';
+export 'widgets/bubble.dart';
 export 'package:provider/provider.dart';
 
-export 'package:pic_note/widgets/label_row.dart';
+export 'widgets/label_row.dart';
+export 'shared/label_dialog.dart';
+export 'package:flutter_slidable/flutter_slidable.dart';
