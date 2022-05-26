727853f3e640fe47304df3084d42db8a326750ac
Prince Emelife <princeemelife@gmail.com>
2022-05-26 01:35:02 +0100

    Screen switch

---
+++ lib/screens/display.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:pic_note/big_screens/home_screen_tab.dart';
+import 'package:pic_note/home_page.dart';
+
+class Display extends StatelessWidget {
+  const Display({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    if (width > 500) {
+      return const HomeTab();
+    } else {
+      return const Home();
+    }
+  }
+}
